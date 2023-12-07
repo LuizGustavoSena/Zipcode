6e667a24ade03d05ed86381f920ff598dfc86c2e
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-12-07 19:22:23 -0300

    test: mock bdClientSpy

--- test/data/mocks/mock-bd.ts
+++ test/data/mocks/mock-bd.ts
@@ -1,43 +1,16 @@
-import { faker } from "@faker-js/faker";
-import { BdClient, RequestHaveUser, ResponseCreateUser } from "../../../src/data/protocols/bd";
-import { User } from "../../../src/domain/models";
+import { BdClient, ResponseGetZipcode } from "../../../src/data/protocols/bd";
+import { RequestGetZipcode, RequestInsertZipcode } from "../../../src/domain/models";
 
 export class BdClientSpy implements BdClient {
-    users: User[] = [];
-    model: string;
-    body: any;
+    zipcodes: string[] = [];
+    email: string;
 
-    async createUser(params: User): Promise<ResponseCreateUser> {
-        this.body = params;
-
-        if (this.users.find(el => el.email === params.email))
-            throw new Error();
-
-        this.users.push(params);
-
-        return {
-            id: faker.word.words(),
-            ...params
-        }
+    async createZipcode(params: RequestInsertZipcode): Promise<void> {
+        this.email = params.email;
+        this.zipcodes.push(params.zipcode);
     }
 
-    async haveUser(params: RequestHaveUser): Promise<boolean> {
-        this.body = params;
-
-        if (this.users.length === 0)
-            return false;
-
-        const haveModel = this.users.find(el => {
-            if (params.password)
-                return el.email === params.email && el.password === params.password;
-
-            return el.email === params.email;
-        });
-
-        if (!haveModel)
-            return false;
-
-        return true;
+    getZipcode(params: RequestGetZipcode): Promise<ResponseGetZipcode> {
+        throw new Error("Method not implemented.");
     }
-
 }
