8ab525aef0c483cf1d42f7a04f2b990f88e40760
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-12-07 19:21:13 -0300

    feat: bdClient interface

--- src/data/protocols/bd/index.ts
+++ src/data/protocols/bd/index.ts
@@ -1,18 +1,10 @@
-import { User } from "../../../domain/models";
+import { RequestGetZipcode, RequestInsertZipcode } from "../../../domain/models";
 
 export interface BdClient {
-    createUser(params: User): Promise<ResponseCreateUser>;
-    haveUser(params: RequestHaveUser): Promise<boolean>
+    createZipcode(params: RequestInsertZipcode): Promise<void>;
+    getZipcode(params: RequestGetZipcode): Promise<ResponseGetZipcode>
 };
 
-export type RequestHaveUser = {
-    email: string;
-    password?: string;
-}
-
-export type ResponseCreateUser = {
-    id: string,
-    username: string,
-    email: string,
-    password: string
+export type ResponseGetZipcode = {
+    zipcodes: string[];
 }
