5c2907faeadfda38e3ac304df1ad732ac70853f6
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-12-08 17:03:43 -0300

    fix: types and interface http

--- src/data/protocols/http/index.ts
+++ src/data/protocols/http/index.ts
@@ -1,5 +1,5 @@
 export interface HttpClient {
-    request<T>(params: Request): Promise<Response<T>>
+    request<T>(params: HttpRequest): Promise<HttpResponse<T>>
 };
 
 export enum HttpStatusCode {
@@ -13,14 +13,22 @@ export enum HttpStatusCode {
     ServerError = 500,
 };
 
-export type Request = {
-    method: 'get' | 'post' | 'put' | 'delete' | 'patch';
+export enum MethodHttp {
+    GET = 'get',
+    POST = 'post',
+    PUT = 'put',
+    DELETE = 'delete',
+    PATCH = 'patch'
+}
+
+export type HttpRequest = {
+    method: MethodHttp;
     url: string;
     body?: any;
     headers?: any
 };
 
-export type Response<T> = {
+export type HttpResponse<T> = {
     statusCode: HttpStatusCode;
-    body: T;
+    body?: T;
 };
