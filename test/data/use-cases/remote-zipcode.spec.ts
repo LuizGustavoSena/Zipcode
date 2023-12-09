0486af9d2bfd7bf7f4b39b9e2f0b110a2e4a47dd
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-12-09 00:14:37 -0300

    test: method getZipcode in RemoteZipcode

--- test/data/use-cases/remote-zipcode.spec.ts
+++ test/data/use-cases/remote-zipcode.spec.ts
@@ -1,32 +1,62 @@
 import { describe, expect, it } from 'vitest';
 import { RemoteZipcode } from "../../../src/data/use-cases/remote-zipcode";
+import { makeResponseGetZipcode } from '../../domain/mocks/get-zipcodes';
 import { makeRequestCreateZipcode } from "../mocks/insert-zipcode";
 import { BdClientSpy } from "../mocks/mock-bd";
+import { HttpClientSpy } from '../mocks/mock-http';
 
 type Props = {
     sut: RemoteZipcode;
     bdClientSpy: BdClientSpy;
+    httpClientSpy: HttpClientSpy
 }
 
 const makeSut = (): Props => {
     const bdClientSpy = new BdClientSpy();
-    const sut = new RemoteZipcode(bdClientSpy);
+    const httpClientSpy = new HttpClientSpy();
+    const sut = new RemoteZipcode(bdClientSpy, httpClientSpy);
 
     return {
         sut,
-        bdClientSpy
+        bdClientSpy,
+        httpClientSpy
     }
 }
 
 describe('RemoteZipcode', () => {
-    it('Should correct create zipcode', () => {
-        const { sut, bdClientSpy } = makeSut();
+    it('Should correct create zipcode', async () => {
+        const { sut, bdClientSpy, httpClientSpy } = makeSut();
 
         const request = makeRequestCreateZipcode();
 
-        sut.insertZipcode(request);
+        httpClientSpy.response.body = makeResponseGetZipcode();
+
+        await sut.insertZipcode(request);
+
+        const response = await sut.getZipcode({ email: request.email });
 
         expect(request.email).toBe(bdClientSpy.email);
         expect(bdClientSpy.zipcodes.includes(request.zipcode)).true;
+
+        expect(response.length).toBe(1);
+        expect(response[0]).not.null.undefined;
+    });
+
+    it('Should correct create another zipcode', async () => {
+        const { sut, httpClientSpy } = makeSut();
+
+        const firstRequest = makeRequestCreateZipcode();
+        const secondRequest = makeRequestCreateZipcode({ email: firstRequest.email });
+
+        httpClientSpy.response.body = makeResponseGetZipcode();
+
+        await sut.insertZipcode(firstRequest);
+        await sut.insertZipcode(secondRequest);
+
+        const response = await sut.getZipcode({ email: firstRequest.email });
+
+        expect(response.length).toBe(2);
+        expect(response[0]).not.null.undefined;
+        expect(response[1]).not.null.undefined;
     })
 })
