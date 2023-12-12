7f6a8f32aae410bba951f5f49e4695e40fd9d774
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-12-12 17:45:46 -0300

    feat: don`t shut down server

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 require('dotenv/config');
 import Fastify from 'fastify';
+import https from 'https';
 import * as ZipcodeControler from './main/controllers/remote-zipcode';
 
 const fastify = Fastify({
@@ -9,6 +10,16 @@ const fastify = Fastify({
 fastify.post('/create_zipcode', ZipcodeControler.createZipcode);
 
 fastify.get('/zipcode', ZipcodeControler.getZipcodes);
+
+fastify.get('/', (_, rep) => {
+    console.log('Ping received');
+
+    rep.send();
+});
+
+setInterval(() => {
+    https.get(process.env.URL_API_ZIPCODE);
+}, Number(process.env.MINUTES_REQUEST) * 60 * 1000);
 
 fastify.listen({
     port: Number(process.env.PORT) || 3000,
