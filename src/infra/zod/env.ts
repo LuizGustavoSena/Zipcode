eb9d5691c2f2211e482c13956348642369846222
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2026-02-17 17:18:31 -0300

    feat: validation env file

---
+++ src/infra/zod/env.ts
@@ -0,0 +1,20 @@
+require('dotenv/config');
+import { z } from "zod";
+import { InvalidEnvError } from "../../domain/error/invalid-env-error";
+
+const schema = z.object({
+    BASE_URL: z.string(),
+    XRAPIDAPIHOST: z.string(),
+    XRAPIDAPIKEY: z.string(),
+    NODE_ENV: z.enum(['dev', 'test', 'prd']),
+    PORT: z.coerce.number().default(3000),
+});
+
+const _env = schema.safeParse({ ...process.env });
+
+if (!_env.success) {
+    const formattedError = (_env as typeof _env & { success: false }).error.format();
+    throw new InvalidEnvError(JSON.stringify(formattedError));
+}
+
+export const env = _env.data;
