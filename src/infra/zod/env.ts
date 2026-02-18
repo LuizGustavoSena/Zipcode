4c2a28be2dc7973188f6f9b8b64501d2b30ebe0a
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2026-02-18 17:51:16 -0300

    feat: auth_url in sample env

--- src/infra/zod/env.ts
+++ src/infra/zod/env.ts
@@ -6,6 +6,7 @@ const schema = z.object({
     BASE_URL: z.string(),
     URL_WEB_SITE: z.string().default('www.website.com'),
     URL_API_ZIPCODE: z.string().default('www.zipcode.com'),
+    AUTH_URL: z.string().default('www.auth.com'),
     XRAPIDAPIHOST: z.string(),
     XRAPIDAPIKEY: z.string(),
     NODE_ENV: z.enum(['dev', 'test', 'prd']),
